06c163d80c13f120b86beb9c631c1177a5266006
linjianghe <linjianghe@tencent.com>
2018-12-19 11:49:10 +0800

    feat: 增加测试用例

--- test/fastest/fastest-core.test.js
+++ test/fastest/fastest-core.test.js
@@ -5,10 +5,7 @@ const fastestCore = require('../../lib/fastest/fastest-core');
 
 const url1 = '/vhost/11_url_cn/vhost/now/activity/spring-festival-2019/index_b4980e63.js?_bid=152';
 const url2 = '/vhost/now_qq_com/cgi-bin/vhost/now/abc';
-
-const pattern1 = '11.url.cn';
-const pattern2 = 'now.qq.com';
-const pattern3 = 'now.qq.com/cgi-bin';
+const url3 = '/not/rewrite/x.js';
 
 const fastestConfig1 = {
     id: 1,
@@ -22,8 +19,8 @@ const fastestConfig1 = {
     ]
 };
 
-describe.only('./lib/fastest/fastest-core.js getRewriteOpts()', () => {
-    it('11.url.cn should return 11_url_cn', () => {
+describe('./lib/fastest/fastest-core.js getRewriteOpts()', () => {
+    it('check fastestConfig1 and url1', () => {
         return fastestCore.getRewriteOpts({
             originDomain: fastestConfig1.originDomain,
             rulesFromCustom: fastestConfig1.rulesFromCustom,
@@ -33,6 +30,34 @@ describe.only('./lib/fastest/fastest-core.js getRewriteOpts()', () => {
                 host: '11.url.cn',
                 url: '/now/activity/spring-festival-2019/index_b4980e63.js?_bid=152',
                 fullUrl: 'http://11.url.cn/now/activity/spring-festival-2019/index_b4980e63.js?_bid=152'
+            });
+        });
+    });
+
+    it('check fastestConfig1 and url2', () => {
+        return fastestCore.getRewriteOpts({
+            originDomain: fastestConfig1.originDomain,
+            rulesFromCustom: fastestConfig1.rulesFromCustom,
+            requestUrl: url2
+        }).then((data) => {
+            expect(data).to.eql({
+                host: 'now.qq.com',
+                url: '/now/abc',
+                fullUrl: 'http://now.qq.com/now/abc'
+            });
+        });
+    });
+
+    it('check fastestConfig1 and url3', () => {
+        return fastestCore.getRewriteOpts({
+            originDomain: fastestConfig1.originDomain,
+            rulesFromCustom: fastestConfig1.rulesFromCustom,
+            requestUrl: url3
+        }).then((data) => {
+            expect(data).to.eql({
+                host: 'now.qq.com',
+                url: '/not/rewrite/x.js',
+                fullUrl: 'http://now.qq.com/not/rewrite/x.js'
             });
         });
     });
