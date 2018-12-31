57f4ad5ad41c86dd8b913542957cd7331d89789b
linjianghe <linjianghe@tencent.com>
2018-12-31 16:15:58 +0800

    fix: 当代理域名和原始域名一致时，则不再设置转发，否则会陷入死循环

--- src/fastest/rulesServerHandler.js
+++ src/fastest/rulesServerHandler.js
@@ -21,7 +21,7 @@ function getRequestParams(ctx) {
 
 exports.handleRequestRules = async (ctx) => {
     // https://github.com/whistle-plugins/whistle.script#%E8%AE%BE%E7%BD%AE%E8%A7%84%E5%88%99
-    console.log('handleRequestRules start', ctx.fullUrl);
+    console.log('\n\nhandleRequestRules start', ctx.fullUrl);
 
     // 获取请求参数
     const requestParams = getRequestParams(ctx);
@@ -33,13 +33,31 @@ exports.handleRequestRules = async (ctx) => {
     // 获得当前的 fastest 配置参数
     const proxyEnv = new ProxyEnv(fastestEnvData);
 
+    console.log('------proxyEnv-----', proxyEnv);
+
     // 完整的规则
     ctx.rules = [
-        // `/^https?://${proxyEnv.proxyDomain}/(.*\.(js|html|css|png|jpg|gif|jpeg|svg|blob|ttf|woff|woff2|mp4).*)$/ fastest://`,
-        `/^https?://${proxyEnv.proxyDomain}/(.*)/ fastest://`,
-        `${proxyEnv.proxyDomain} ${proxyEnv.originDomain}`,
         ...proxyEnv.formatRules()
     ];
+
+    // 正常情况下两者都是不一样的，但不排除意外场景
+    if (proxyEnv.proxyDomain !== proxyEnv.originDomain) {
+        ctx.rules = [
+            // `/^https?://${proxyEnv.proxyDomain}/(.*\.(js|html|css|png|jpg|gif|jpeg|svg|blob|ttf|woff|woff2|mp4).*)$/ fastest://`,
+            `/^https?://${proxyEnv.proxyDomain}/(.*)/ fastest://`,
+            `${proxyEnv.proxyDomain} ${proxyEnv.originDomain}`
+        ].concat(ctx.rules);
+    }
+
+    // ctx.rules = [
+    //     '/^https?://fastest2.now.qq.com/(.*)/ fastest://',
+    //     'fastest2.now.qq.com now.qq.com',
+    //     'now.qq.com/cgi-bin 10.100.64.201',
+    //     '11.url.cn 10.100.64.201',
+    //     'now.qq.com 10.100.64.201'
+    // ];
+
+    console.log('---ctx.rules---', ctx.rules);
 
     // 将配置参数传递下去，后续流程需要用到这些配置
     ctx.options.proxyEnv = proxyEnv;
