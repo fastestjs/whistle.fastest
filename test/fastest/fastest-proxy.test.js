aa8b8a8d864e6267b49e56ff5b95920b7cae9eb4
linjianghe <linjianghe@tencent.com>
2019-03-17 18:50:37 +0800

    refactor: 补充了 fastest-proxy.test.js 的用例

--- test/fastest/fastest-proxy.test.js
+++ test/fastest/fastest-proxy.test.js
@@ -3,45 +3,51 @@ const expect = chai.expect;
 
 const fastestProxy = require('../../lib/fastest/fastest-proxy');
 
-const url1 = 'http://fastest2.now.qq.com/vhost/11_url_cn/vhost/now/activity/spring-festival-2019/index_b4980e63.js?_bid=152';
-const url2 = 'http://fastest2.now.qq.com/vhost/now_qq_com/cgi-bin/vhost/now/abc';
+const url1 = 'http://fastest2.now.qq.com/_fst_/11_url_cn/_fst_/now/activity/spring-festival-2019/index_b4980e63.js?_bid=152';
+const url2 = 'http://fastest2.now.qq.com/_fst_/now_qq_com/cgi-bin/_fst_/now/abc';
 
 const pattern1 = '11.url.cn';
 const pattern2 = 'now.qq.com';
 const pattern3 = 'now.qq.com/cgi-bin';
 
-describe('./lib/fastest/fastest-proxy.js isMatchVHost()', () => {
-    it('check url1 & pattern1 should return true', () => {
-        expect(fastestProxy.isMatchVHost(url1, pattern1)).to.be.true;
-    });
-
-    it('check url1 & pattern2 should return false', () => {
-        expect(fastestProxy.isMatchVHost(url1, pattern2)).to.be.false;
-    });
-
-    it('check url2 & pattern2 should return false', () => {
-        expect(fastestProxy.isMatchVHost(url2, pattern2)).to.be.false;
-    });
-
-    it('check url2 & pattern3 should return true', () => {
-        expect(fastestProxy.isMatchVHost(url2, pattern3)).to.be.true;
+describe('./lib/fastest/fastest-proxy.js MATCH_PROXY_TAG', () => {
+    it('MATCH_PROXY_TAG is _fst_', () => {
+        expect(fastestProxy.MATCH_PROXY_TAG).to.equal('_fst_');
     });
 });
 
-describe('./lib/fastest/fastest-proxy.js removeVHost()', () => {
+describe('./lib/fastest/fastest-proxy.js isMatchVProxy()', () => {
+    it('check url1 & pattern1 should return true', () => {
+        expect(fastestProxy.isMatchVProxy(url1, pattern1)).to.be.true;
+    });
+
+    it('check url1 & pattern2 should return false', () => {
+        expect(fastestProxy.isMatchVProxy(url1, pattern2)).to.be.false;
+    });
+
+    it('check url2 & pattern2 should return false', () => {
+        expect(fastestProxy.isMatchVProxy(url2, pattern2)).to.be.false;
+    });
+
+    it('check url2 & pattern3 should return true', () => {
+        expect(fastestProxy.isMatchVProxy(url2, pattern3)).to.be.true;
+    });
+});
+
+describe('./lib/fastest/fastest-proxy.js removeVProxy()', () => {
     it('check url1 and pattern1', () => {
-        expect(fastestProxy.removeVHost(url1, pattern1)).to.equal('http://fastest2.now.qq.com/now/activity/spring-festival-2019/index_b4980e63.js?_bid=152');
+        expect(fastestProxy.removeVProxy(url1, pattern1)).to.equal('http://fastest2.now.qq.com/now/activity/spring-festival-2019/index_b4980e63.js?_bid=152');
     });
 
     it('check url1 and pattern2', () => {
-        expect(fastestProxy.removeVHost(url1, pattern2)).to.equal(url1);
+        expect(fastestProxy.removeVProxy(url1, pattern2)).to.equal(url1);
     });
 
     it('check url2 and pattern2', () => {
-        expect(fastestProxy.removeVHost(url2, pattern2)).to.equal(url2);
+        expect(fastestProxy.removeVProxy(url2, pattern2)).to.equal(url2);
     });
 
     it('check url2 and pattern3', () => {
-        expect(fastestProxy.removeVHost(url2, pattern3)).to.equal('http://fastest2.now.qq.com/now/abc');
+        expect(fastestProxy.removeVProxy(url2, pattern3)).to.equal('http://fastest2.now.qq.com/now/abc');
     });
 });
