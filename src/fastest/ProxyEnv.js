c73fe2629f54853e1c0fff380e13dbdb79c800ae
linjianghe <linjianghe@tencent.com>
2019-03-17 20:00:12 +0800

    refactor: 注释

--- src/fastest/ProxyEnv.js
+++ src/fastest/ProxyEnv.js
@@ -4,28 +4,31 @@ class ProxyEnv {
     constructor(data = {}) {
         /**
          * 配置环境的ID，可通过这个ID来快速选择到这个测试环境
+         * @type {Number}
          */
         this.id = data.id;
 
         /**
-         * 配置环境的别称，用于在选择时进行识别
+         * 配置环境的别称，一般建议中文名，用于在选择环境时进行识别
+         * @type {String}
          */
         this.name = data.name || '';
 
         /**
          * 正式环境的原始域名
+         * @type {String}
          */
         this.originDomain = data.origin_domain;
 
         /**
          * 测试环境的代理域名
-         * @type {string}
+         * @type {String}
          */
         this.proxyDomain = data.proxy_domain;
 
         /**
-         * 本地的 whistle 服务地址，例如 http://127.0.0.1:8080
-         * @type {string}
+         * 本地的 whistle 服务地址，例如 http://127.0.0.1:8080，用于代理转发时设置的代理服务地址
+         * @type {String}
          */
         this.whistleServer = data.whistle_server;
 
@@ -37,10 +40,16 @@ class ProxyEnv {
 
         /**
          * 代理规则列表
+         * @type {ProxyRule[]}
          */
         this.rules = this._getRules(data.rules);
     }
 
+    /**
+     * 获得 whistle 规则
+     * 例如：['now.qq.com 10.100.11.11','11.url.cn 10.100.11.11']
+     * @return {String[]}
+     */
     formatRules() {
         return this.rules.map(rule => {
             return `${rule.pattern} ${rule.operatorURI}`;
--- src/fastest/rulesServerHandler.js
+++ src/fastest/rulesServerHandler.js
@@ -21,11 +21,11 @@ function getRequestParams(ctx) {
 
 exports.handleRequestRules = async (ctx) => {
     // https://github.com/whistle-plugins/whistle.script#%E8%AE%BE%E7%BD%AE%E8%A7%84%E5%88%99
-    console.log('\n\nhandleRequestRules start', ctx.fullUrl);
+    console.log('\n\n[rulesServerHandler.js] handleRequestRules(ctx) start', ctx.fullUrl);
 
     // 获取请求参数
     const requestParams = getRequestParams(ctx);
-    console.log('handleRequestRules requestParams', requestParams);
+    console.log('[rulesServerHandler.js] handleRequestRules(ctx) requestParams', requestParams);
 
     // 请求配置管理页的数据
     const fastestEnvData = db.getRemoteConfig(requestParams.proxyEnvId, requestParams.proxyDomain, { uin: requestParams.uin });
@@ -57,10 +57,10 @@ exports.handleRequestRules = async (ctx) => {
     //     'now.qq.com 10.100.64.201'
     // ];
 
-    console.log('---ctx.rules---', ctx.rules);
+    console.log('[rulesServerHandler.js] handleRequestRules(ctx) ---ctx.rules---', ctx.rules);
 
     // 将配置参数传递下去，后续流程需要用到这些配置
     ctx.options.proxyEnv = proxyEnv;
 
-    console.log('handleRequestRules end');
+    console.log('[rulesServerHandler.js] handleRequestRules(ctx) end');
 };
